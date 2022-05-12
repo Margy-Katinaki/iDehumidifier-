3e28799905fccea72c18feeed724d68531bd06b8
Margy <margy2katnk@gmail.com>
2022-05-13 01:09:31 +0300

    Add xml files and vector asset
    Signed-off-by: Margy <margy2katnk@gmail.com>

--- app/src/main/java/com/example/idehumidifier/MainActivity.kt
+++ app/src/main/java/com/example/idehumidifier/MainActivity.kt
@@ -9,24 +9,24 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.mode)
 
-        val list = mutableListOf<Dehumidifier>()
+//        val list = mutableListOf<Dehumidifier>()
+//
+//        val a = Dehumidifier(1, "kati", "kati model", 100.0, 88.0)
+//        val b = Dehumidifier(2, "kati", "kati model", 100.0, 88.0)
+//        val c = Dehumidifier(3, "kati", "kati model", 100.0, 88.0)
+//        val d = Dehumidifier(4, "kati", "kati model", 100.0, 88.0)
+//
+//        list.add(a)
+//        list.add(b)
+//        list.add(c)
+//        list.add(d)
 
-        val a = Dehumidifier(1, "kati", "kati model", 100.0, 88.0)
-        val b = Dehumidifier(2, "kati", "kati model", 100.0, 88.0)
-        val c = Dehumidifier(3, "kati", "kati model", 100.0, 88.0)
-        val d = Dehumidifier(4, "kati", "kati model", 100.0, 88.0)
-
-        list.add(a)
-        list.add(b)
-        list.add(c)
-        list.add(d)
-
-        val adapter = DehumidifierAdapter(list)
-
-        recycler_view.adapter = adapter
-        recycler_view.layoutManager = LinearLayoutManager(this)
+//        val adapter = DehumidifierAdapter(list)
+//
+//        recycler_view.adapter = adapter
+//        recycler_view.layoutManager = LinearLayoutManager(this)
 
 
 
